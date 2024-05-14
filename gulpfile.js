87dfa12d2ad6fb837cf0d371af59859e3d81082a
Elena <pumpkinpum@gmail.com>
2024-05-14 13:19:14 +0200

    feat: add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,33 @@ import browser from 'browser-sync';
 
 // Styles
 export const styles = () => {
-  return gulp.src('source/css/style.css', { sourcemaps: true })
+  return gulp
+    .src('source/css/*.css', {sourcemaps: true})
     .pipe(plumber())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
-    .pipe(browser.stream()); 
-}
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest('dist/css', {sourcemaps: '.'}))
+    .pipe(browser.stream());
+};
 
 // Server
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'source',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 // Watcher
 const watcher = () => {
-  gulp.watch('source/sass/**/*.css', gulp.series(styles));
+  gulp.watch('source/css/**/*.css', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
-  gulp.watch('source/js/**/*.js').on('change', browser.reload); 
-}
+  gulp.watch('source/js/**/*.js').on('change', browser.reload);
+};
 
 // Default Task
-export default gulp.series(
-  styles, server, watcher
-);
+export default gulp.series(styles, server, watcher);
